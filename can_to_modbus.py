d89a498679a315e385f50d30de86a26262646261
Rikus-C <rcoetzee967@gmail.com>
2023-10-05 21:23:47 +0200

    some more stuff added

--- can_to_modbus.py
+++ can_to_modbus.py
@@ -2,6 +2,10 @@ import json
 from time import sleep
 from threading import Thread
 
+# custom libraries
+import tcp_client
+from modbus import create_modbus_frame
+
 # import settings
 settings = json.load(open("settings.json", "r"))
 
@@ -14,15 +18,16 @@ t2_healthy = False
 def read_usb():
   # connect to can receiver via usb port
   while True:
-    t1_healthy = True
-    print("cool")
+    # t1_healthy = True
+    temp = 10
 
 # create thread that takes oldest item in the buffer, converts it to modbus and sends it to the drive
 def write_to_plc():
   # connect to plc via tcp client
+  tcp_client.connect_to_tcp_server();
   while len(buffer) > 1:
-    t2_healthy = True
-    print("dude")
+    # t2_healthy = True
+    temp = 10
 
 # create a function that checks the health of the threads above
 def check_threads_health():
@@ -44,11 +49,11 @@ def check_threads_health():
 read_usb_thread = Thread(target = read_usb)
 write_to_plc_thread = Thread(target = write_to_plc)
 
-# read_usb_thread.start();
-# write_to_plc_thread.start();
+# detach threads 
+read_usb_thread.daemon = True
+write_to_plc_thread.daemon = True
 
-# # detach threads 
-# read_usb_thread.daemon = True
-# write_to_plc_thread.daemon = True
+read_usb_thread.start();
+write_to_plc_thread.start();
 
 check_threads_health()
--- tcp_client.py
+++ tcp_client.py
@@ -1,5 +1,23 @@
+import json
+import socket
+
+settings = json.load(open("settings.json", "r"))
+
+# Server IP address and port
+server_ip = settings["plc ip"]  
+server_port = settings["plc port"] 
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 def connect_to_tcp_server():
+  client_socket.connect((server_ip, server_port))
   return 0
 
+def close_tcp_connection():
+  # Close the socket
+  client_socket.close()
+
 def send_message_to_plc():
+  message = "";
+  client_socket.send(message.encode())
   return 0
