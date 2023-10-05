258b6181c6b1b7f91c2aa03d0db3277104641478
F-Coetzee-MD <fcoetzee@masterdrilling.com>
2023-10-05 14:24:39 +0200

    first

---
+++ can_to_modbus.py
@@ -0,0 +1,9 @@
+
+
+# import settings
+
+buffer = [];
+
+# create thread that received usb can messages and stores it in buffer
+
+# create thread that takes oldest item in the buffer, converts it to modbus and sends it to the drive
